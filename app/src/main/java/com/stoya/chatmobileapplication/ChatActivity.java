868c0992e041129e2172de1c4c653d06c6457700
sLeytern <stoyanlazarov2004@gmail.com>
2026-04-20 19:00:29 +0300

    Оправяне на функционалността на чата между хора в реално време.

--- app/src/main/java/com/stoya/chatmobileapplication/ChatActivity.java
+++ app/src/main/java/com/stoya/chatmobileapplication/ChatActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,13 +13,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.stoya.chatmobileapplication.adapter.ChatRecyclerAdapter;
+import com.stoya.chatmobileapplication.adapter.SearchUserRecyclerAdapter;
 import com.stoya.chatmobileapplication.model.ChatMessageModel;
 import com.stoya.chatmobileapplication.model.ChatRoomModel;
 import com.stoya.chatmobileapplication.model.UserModel;
@@ -34,6 +40,7 @@ public class ChatActivity extends AppCompatActivity {
     UserModel otherUser;
     String chatRoomId;
     ChatRoomModel chatRoomModel;
+    ChatRecyclerAdapter adapter;
     EditText messageInput;
     ImageButton sendMessageBtn;
     ImageButton backBtn;
@@ -74,7 +81,30 @@ public class ChatActivity extends AppCompatActivity {
         }));
 
         getOrCreateChatRoomModel();
+        setupChatRecyclerView();
+    }
 
+    void setupChatRecyclerView() {
+
+        Query query = FirebaseUtil.getChatRoomMessageReference(chatRoomId)
+                .orderBy("timestamp", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<ChatMessageModel> options = new FirestoreRecyclerOptions.Builder<ChatMessageModel>()
+                .setQuery(query, ChatMessageModel.class).build();
+
+        adapter = new ChatRecyclerAdapter(options, getApplicationContext());
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setReverseLayout(true);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+                recyclerView.smoothScrollToPosition(0);
+            }
+        });
     }
 
     void sendMessageToUser(String message) {
